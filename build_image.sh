024d401880b61de34ce0cd943131f20262258520
zorbaTheRainy <143339912+zorbaTheRainy@users.noreply.github.com>
2024-02-24 11:09:58 +0100

    Update build_image.sh
    typo

--- build_image.sh
+++ build_image.sh
@@ -8,9 +8,10 @@ TAG=$(curl https://nxfilter.org/curver.php)
 # Check if the tag exists
 if docker manifest inspect ${REPO}:${TAG} >/dev/null 2>&1; then
     echo "Tag ${TAG} exists in repository ${REPO}."
-    echo "Existing ..."
+    echo "Exiting ..."
 else
-    echo "Tag ${TAG} does not exist in repository ${REPO}."
+    echo "Tag ${TAG} does NOT exist in repository ${REPO}."
+    echo "Building ..."
     # sudo docker buildx build --build-arg VER_NUM=${TAG} --platform=amd64,arm64 -t ${REPO}:${TAG}  .
     sudo docker build --build-arg VER_NUM=${TAG}  -t ${REPO}:${TAG} .
     docker push ${REPO}:${TAG}
